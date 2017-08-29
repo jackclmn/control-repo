87e1ea2c5408f0eb8236830780567ac0f425adb6
Jack Coleman <jackcoleman@Jacks-MacBook-Pro.local>
2017-08-29 16:44:29 -0400

    Added webserver role

--- site/role/manifests/webserver.pp
+++ site/role/manifests/webserver.pp
@@ -1,7 +1,11 @@
+# Webserver Role
 class role::webserver {
-
-  #This role would be made of all the profiles that need to be included to make a webserver work
-  #All roles should include the base profile
   include profile::base
+  include profile::apache
+  include profile::mysql
+
+  Class[profile::base]
+  -> Class[profile::apache]
+  -> Class[profile::mysql]
 
 }
