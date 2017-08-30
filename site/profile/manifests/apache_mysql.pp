3cb557cba185cc23feffe954d7319bd94ebe179f
Jack Coleman <jackcoleman@Jacks-MacBook-Pro.local>
2017-08-30 13:53:30 -0400

    added apache to profile

--- site/profile/manifests/apache_mysql.pp
+++ site/profile/manifests/apache_mysql.pp
@@ -1,8 +1,15 @@
 # class profile::apache_mysql
-class profile::apache_mysql (
-  $vhosts = $profile::apache::vhosts,
-  ) {
+class profile::apache_mysql {
+  $vhosts = {
+    'vhost1' => { port => 80, docroot => '/var/www1'},
+    'vhost2' => { port => 8080, docroot => '/var/www2'},
+    'vhost3' => { port => 9090, docroot => '/var/www3'},
+    'vhost4' => { port => 9000, docroot => '/var/www4'},
+    'vhost5' => { port => 8000, docroot => '/var/www5'},
+  }
+  $dbs = {
 
+  }
   class { 'apache-mysql-manager':
     vhosts => $vhosts,
     dbs    => $dbs,
