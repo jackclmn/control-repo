624842296012897132c1d6ac0edffad301286576
Jack Coleman <jackcoleman@Jacks-MacBook-Pro.local>
2017-08-30 16:40:20 -0400

    add mysql dbs

--- site/profile/manifests/apache_mysql.pp
+++ site/profile/manifests/apache_mysql.pp
@@ -7,8 +7,16 @@ class profile::apache_mysql {
     'vhost4' => { port => 8009, docroot => '/var/www4'},
     'vhost5' => { port => 9000, docroot => '/var/www5'},
   }
+  $dbs = {
+    'db1' => {},
+    'db2' => {},
+    'db3' => {},
+    'db4' => {},
+    'db5' => {},
+  }
   class { 'apache_mysql_manager':
     vhosts => $vhosts,
+    dbs    => $dbs,
   }
   contain apache_mysql_manager
 }
