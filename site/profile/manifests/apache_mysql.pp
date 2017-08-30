aca5fee138715b49c8daecf6937db711c9031f53
Jack Coleman <jackcoleman@Jacks-MacBook-Pro.local>
2017-08-30 14:55:54 -0400

    include manager

--- site/profile/manifests/apache_mysql.pp
+++ site/profile/manifests/apache_mysql.pp
@@ -7,8 +7,8 @@ class profile::apache_mysql {
     'vhost4' => { port => 9000, docroot => '/var/www4'},
     'vhost5' => { port => 8000, docroot => '/var/www5'},
   }
-
-  class { 'apache_mysql_manager':
-    vhosts => $vhosts,
-  }
+  include apache_mysql_manager
+  #class { 'apache_mysql_manager':
+  #  vhosts => $vhosts,
+  #}
 }
