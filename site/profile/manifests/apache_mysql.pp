7d5e80da734f0817d0bbc3a2e31d5e7ae01e0188
Jack Coleman <jackcoleman@Jacks-MacBook-Pro.local>
2017-08-30 16:30:07 -0400

    8009, 9000

--- site/profile/manifests/apache_mysql.pp
+++ site/profile/manifests/apache_mysql.pp
@@ -4,8 +4,8 @@ class profile::apache_mysql {
     'vhost1' => { port => 80, docroot => '/var/www1'},
     'vhost2' => { port => 81, docroot => '/var/www2'},
     'vhost3' => { port => 8008, docroot => '/var/www3'},
-    #'vhost4' => { port => 9000, docroot => '/var/www4'},
-    #'vhost5' => { port => 2000, docroot => '/var/www5'},
+    'vhost4' => { port => 8009, docroot => '/var/www4'},
+    'vhost5' => { port => 9000, docroot => '/var/www5'},
   }
   class { 'apache_mysql_manager':
     vhosts => $vhosts,
