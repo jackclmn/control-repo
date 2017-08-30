e9900ce7f5bcf93356c2cd6e9a71eb3c71251863
Jack Coleman <jackcoleman@Jacks-MacBook-Pro.local>
2017-08-30 16:12:00 -0400

    new port

--- site/profile/manifests/apache_mysql.pp
+++ site/profile/manifests/apache_mysql.pp
@@ -5,7 +5,7 @@ class profile::apache_mysql {
     'vhost2' => { port => 8080, docroot => '/var/www2'},
     'vhost3' => { port => 9090, docroot => '/var/www3'},
     'vhost4' => { port => 9000, docroot => '/var/www4'},
-    'vhost5' => { port => 8000, docroot => '/var/www5'},
+    'vhost5' => { port => 9001, docroot => '/var/www5'},
   }
   class { 'apache_mysql_manager':
     vhosts => $vhosts,
