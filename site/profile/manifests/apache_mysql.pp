fb30487841e8bef91e01b23596e11301e0905345
Jack Coleman <jackcoleman@Jacks-MacBook-Pro.local>
2017-08-30 16:24:28 -0400

    81

--- site/profile/manifests/apache_mysql.pp
+++ site/profile/manifests/apache_mysql.pp
@@ -2,7 +2,7 @@
 class profile::apache_mysql {
   $vhosts = {
     'vhost1' => { port => 80, docroot => '/var/www1'},
-    #'vhost2' => { port => 8080, docroot => '/var/www2'},
+    'vhost2' => { port => 81, docroot => '/var/www2'},
     #'vhost3' => { port => 2500, docroot => '/var/www3'},
     #'vhost4' => { port => 9000, docroot => '/var/www4'},
     #'vhost5' => { port => 2000, docroot => '/var/www5'},
