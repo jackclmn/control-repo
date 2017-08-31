d62a0080f626e88c13be4a8a53d8d5df4d157fff
Jack Coleman <jackcoleman@Jacks-MacBook-Pro.local>
2017-08-31 11:32:43 -0400

    hiera for mysql

--- site/profile/manifests/apache_mysql.pp
+++ site/profile/manifests/apache_mysql.pp
@@ -1,17 +1,12 @@
 # class profile::apache_mysql
 class profile::apache_mysql {
   $vhosts = lookup(profile::apache_mysql::vhosts)
-  $dbs = {
-    'db1' => {},
-    'db2' => {},
-    'db3' => {},
-    'db4' => {},
-    'db5' => {},
-  }
+  $dbs = lookup(profile::apache_mysql::dbs)
+
   class { 'apache_mysql_manager':
     vhosts => $vhosts,
     dbs    => $dbs,
   }
   contain apache_mysql_manager
-  #commenttsssss
+
 }
