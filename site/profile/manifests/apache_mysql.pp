250358a78702b1d84401730da6d6a2f8c0997768
Jack Coleman <jackcoleman@Jacks-MacBook-Pro.local>
2017-09-01 10:35:59 -0400

    scope

--- site/profile/manifests/apache_mysql.pp
+++ site/profile/manifests/apache_mysql.pp
@@ -1,7 +1,7 @@
 # class profile::apache_mysql
 class profile::apache_mysql {
-  $vhosts = lookup(::profile::apache_mysql::vhosts)
-  $dbs = lookup(::profile::apache_mysql::dbs)
+  $vhosts = lookup(profile::apache_mysql::vhosts)
+  $dbs = lookup(profile::apache_mysql::dbs)
 
   class { 'apache_mysql_manager':
     vhosts => $vhosts,
