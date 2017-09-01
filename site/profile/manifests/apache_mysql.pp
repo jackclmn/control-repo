7c4a8dda6dc5289812ce0bcfe27a5ee2c3e781d3
Jack Coleman <jackcoleman@Jacks-MacBook-Pro.local>
2017-09-01 14:26:25 -0400

    eyaml

--- site/profile/manifests/apache_mysql.pp
+++ site/profile/manifests/apache_mysql.pp
@@ -1,6 +1,8 @@
 # class profile::apache_mysql
 class profile::apache_mysql {
-
+  notify { 'test':
+    message => hiera('message'),
+  }
   $vhosts = hiera_hash('vhosts')
   $dbs = hiera_hash('dbs')
 
