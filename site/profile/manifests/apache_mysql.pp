266ec3f75b9a37200d80202d6c9bed7d1003afdb
Jack Coleman <jackcoleman@Jacks-MacBook-Pro.local>
2017-09-11 11:44:52 -0400

    remove test

--- site/profile/manifests/apache_mysql.pp
+++ site/profile/manifests/apache_mysql.pp
@@ -1,8 +1,5 @@
 # class profile::apache_mysql
 class profile::apache_mysql {
-  notify { 'test':
-    message => hiera('message'),
-  }
   $vhosts = hiera_hash('vhosts')
   $dbs = hiera_hash('dbs')
 
